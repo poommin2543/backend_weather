ec4de977095235647d3093955fb9e3ecc26db92f
poommmin2543 <poommin2543@gmail.com>
2025-01-26 17:50:47 +0700

    add script

---
+++ render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y \
+    wget \
+    ca-certificates \
+    fonts-liberation \
+    libappindicator3-1 \
+    libnss3 \
+    lsb-release \
+    xdg-utils
